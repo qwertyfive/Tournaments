1a0bf966488b4b812d315aeb96c7ea8026f2a32e
qwertyfive <39852023+qwertyfive@users.noreply.github.com>
2021-06-17 20:49:36 +0300

    Add files via upload

---
+++ RoleInitializer.cs
@@ -0,0 +1,33 @@
+﻿using MyTournaments.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace MyTournaments
+{
+    public class RoleInitializer
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            string adminEmail = "zatiliukandrii@gmail.com";
+            string adminUserName = "Admin";
+            string password = "54859871";
+            if (await roleManager.FindByNameAsync("admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("admin"));
+            }
+            if (await roleManager.FindByNameAsync("user") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("user"));
+            }
+            if (await userManager.FindByNameAsync(adminEmail) == null)
+            {
+                User admin = new User { Email = adminEmail, UserName = adminEmail, Name = adminUserName };
+                IdentityResult result = await userManager.CreateAsync(admin, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "user");
+                }
+            }
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+/* begin Scroll Down Button */
+(function () {
+    'use strict';
+
+    var btnScrollDown = document.querySelector('#scroll_down');
+
+    function scrollDown() {
+        var windowCoords = document.documentElement.clientHeight;
+        (function scroll() {
+            if (window.pageYOffset < windowCoords) {
+                window.scrollBy(0, 10);
+                setTimeout(scroll, 0);
+            }
+            if (window.pageYOffset > windowCoords) {
+                window.scrollTo(0, windowCoords+1);
+            }
+        })();
+    }
+
+    btnScrollDown.addEventListener('click', scrollDown);
+})();
+/* end Scroll Down Button */
+
+// Write your JavaScript code.
